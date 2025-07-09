b340d8ffd737ca1233dd93af226176145dc7a51b
Your Name <lucascampanharo19@gmail.com>
2025-07-09 10:37:07 -0300

    done homepage

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import { HomePage } from "./pages/home/home";
+
+export function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import "./home.css";
+
+export function HomePage() {
+  // const irParaProdutos = () =>
+  return (
+    <main>
+      <h1 class="titulo" title="Clique para acessar os produtos.">
+        MERCADINHO SASA
+      </h1>
+    </main>
+  );
+}
